3b46e4d989a72678c8512f626485d64842c70613
timo777 <31273061+timo777@users.noreply.github.com>
2017-09-17 18:40:36 +0200

    Add files via upload

---
+++ Ugani stevilko.py
@@ -0,0 +1,12 @@
+
+userInput = raw_input("Vtipkaj skrivno stevilko med 1 ali 10: ")
+
+Secret = 8
+
+if ( 2 == int ( userInput ) ) :
+
+    userMessage = "Congrats"
+
+else : userMessage = " try again looser "
+
+print userMessage
