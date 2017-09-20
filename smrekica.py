bbf9c99a634123f8623ea0aaa0090c57a437a518
timo777 <31273061+timo777@users.noreply.github.com>
2017-09-20 17:15:32 +0200

    Add files via upload
    Dekoracijo popolnoma zgresena vendar sem dal notri samo da pokazem da razumem stringe

---
+++ smrekica.py
@@ -0,0 +1,11 @@
+value = int(raw_input("Vnesi smreko:" ))
+x = 0
+
+
+for x in range (value):
+    x += 1
+    y = value - x
+    c = ((x - 1) * 2)
+    a = ()
+    print " " * y + "4" + "*" * (1 + c) + "3"
+
